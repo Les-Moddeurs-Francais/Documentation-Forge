d4d2654833b1f8123fd145427aa185221633e065
Mysterious_Dev <40738104+Mysterious-Dev@users.noreply.github.com>
2021-09-03 17:02:00 +0200

    Edit Main Page (#22)

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -3,59 +3,45 @@ import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
-  {
-    title: 'Open Source',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Tout le monde peut contribuer à cette documentation par Github.
-      </>
-    ),
-  },
-  {
-    title: 'Multi-Version',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Documentation pouvant prendre en compte les 3 dernières versions de Minecraft sorties sous Forge (WIP).
-      </>
-    ),
-  },
-  {
-    title: '┬─┬ ノ( ゜-゜ノ)',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        ...
-      </>
-    ),
-  },
+    {
+        title: 'Mysterious_Dev',
+        img: 'https://avatars.githubusercontent.com/u/40738104?v=4',
+    },
+    {
+        title: 'Zom',
+        img: 'https://avatars.githubusercontent.com/u/24624348?v=4',
+    },
+    {
+        title: 'Moddeur_Ultime',
+        img: '../../static/img/le_moddeur_ultime.jpg',
+    }
 ];
 
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({img, title, description}) {
+    return (
+        <div className={clsx('col col--4')}>
+            <div className="text--center">
+                <img src={img} className={styles.featureSvg} alt={title} />
+            </div>
+            <div className="text--center padding-horiz--md">
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <h2 className="text--center">Équipe LMF</h2>
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
