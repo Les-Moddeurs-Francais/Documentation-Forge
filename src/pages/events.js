a238ec4056105ec1251cd3e4252bc7587ffc3f98
Mysterious_Dev <40738104+Mysterious-Dev@users.noreply.github.com>
2021-09-04 13:25:47 +0200

    Begin events page

---
+++ src/pages/events.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+function Events() {
+    return (
+        <Layout title="Liste des events">
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '50vh',
+                    fontSize: '20px',
+                }}>
+                <p>
+                    En construction
+                </p>
+            </div>
+        </Layout>
+    );
+}
+
+export default Events;
