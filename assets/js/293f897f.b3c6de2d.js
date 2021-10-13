4157a5c5be94f9f4405830e02f5fbc83428bdd4e
Mysterious-Dev <40738104+Mysterious-Dev@users.noreply.github.com>
2021-10-13 09:48:23 +0000

    Deploying to gh-pages from @ Les-Moddeurs-Francais/Forge-Doc@29284ce72ffda2ca801e5dcf8fdb18936d066a83 🚀

---
+++ assets/js/293f897f.b3c6de2d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdoc_forge=self.webpackChunkdoc_forge||[]).push([[5659],{1555:function(e){e.exports=JSON.parse('{"permalink":"/news","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
