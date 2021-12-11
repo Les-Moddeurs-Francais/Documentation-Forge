445f2a7ad30cdf9b00f8c7667dc9672208887404
Mysterious-Dev <40738104+Mysterious-Dev@users.noreply.github.com>
2021-12-11 21:35:12 +0000

    Deploying to gh-pages from @ Les-Moddeurs-Francais/Forge-Doc@52f4e79293808915e04b3cdf502e8715f82c89a2 🚀

---
+++ assets/js/6e29c054.03b52a17.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdoc_forge=self.webpackChunkdoc_forge||[]).push([[8045],{862:function(o){o.exports=JSON.parse('{"name":"color","permalink":"/docs/1.17/tags/color","docs":[{"id":"version-1.17/items/colored","title":"Item color\xe9","description":"Comment cr\xe9er un item color\xe9 ?","permalink":"/docs/1.17/items/colored"}],"allTagsPath":"/docs/1.17/tags"}')}}]);
