b3588258fa0cf611af7fc46134e6b63c15825e83
Mysterious_Dev <40738104+Mysterious-Dev@users.noreply.github.com>
2022-05-10 15:20:51 +0200

    Custom NotFound (404 error) component (#116)
    * Custom NotFound (404 error) component
    
    * Correct mistake
    
    * Add button to go back on home

---
+++ src/theme/NotFound.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import Translate, {translate} from '@docusaurus/Translate';
+import {PageMetadata} from '@docusaurus/theme-common';
+import Link from "@docusaurus/core/lib/client/exports/Link";
+export default function NotFound() {
+  return (
+    <>
+      <PageMetadata
+        title={translate({
+          id: 'theme.NotFound.title',
+          message: 'Page Not Found',
+        })}
+      />
+      <Layout>
+        <main className="container margin-vert--xl">
+          <div className="row">
+            <div className="col col--6 col--offset-3">
+              <h1 className="hero__title">
+                Oups 🙄
+              </h1>
+              <p>
+                  Il semble que nous ayons peut-être oublié cette page 👀
+              </p>
+              <p>
+                  Si c'est le cas, n'hésitez pas à nous contacter sur <a href="https://discord.lesmoddeursfrancais.fr">Discord</a> 😊.
+              </p>
+                <Link
+                    className="button button--secondary button--lg"
+                    to="/">
+                    Revenir à l'accueil
+                </Link>
+            </div>
+          </div>
+        </main>
+      </Layout>
+    </>
+  );
+}
