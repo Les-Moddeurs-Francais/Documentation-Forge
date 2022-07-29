a2378e8582d8f72a69dd8368b7c9a294c0d6e516
Mysterious-Dev <40738104+Mysterious-Dev@users.noreply.github.com>
2022-07-29 14:54:42 +0000

    Deploying to gh-pages from @ Les-Moddeurs-Francais/Documentation-Forge@3fad06e92ee8d226deb4f8d264a2bbee3525d516 🚀

---
+++ assets/js/8b0f4364.af16cd36.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdoc_forge=self.webpackChunkdoc_forge||[]).push([[2701],{183:function(e){e.exports=JSON.parse('{"label":"update","permalink":"/news/tags/update","allTagsPath":"/news/tags","count":10}')}}]);
